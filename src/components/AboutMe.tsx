7186280aa38423813ffda61185f51864e20fdab5
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-10-02 14:41:06 +0500

    ssda

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,35 +4,35 @@ import aboutImg from "/public/banner.png"
 import Image from "next/image"
 const AboutMe = () => {
     return (
-        <section id="about"  className='relative my-28 h-auto'>
+        <section id="about" className='relative my-28 h-auto'>
             <div className='max-w-[92%] mx-auto'>
                 <div className='flex items-center justify-end gap-24'>
                     <div className='md:block hidden absolute -top-36 left-20'>
-                        <Image src={aboutImg} alt='img'/>
+                        <Image src={aboutImg} alt='img' />
                     </div>
                     <div className='md:max-w-[50%] max-w-[95%] w-full relative'>
                         <h2 className='z-[-1] absolute top-0 left-0 right-0 md:text-8xl text-trans'>About</h2>
                         <h2 className='text-3xl md:text-5xl'>About Me</h2>
-                        <p className='mt-6 md:mt-10 text-gray-400'>Hello! I am Front End Developer. I have one year of experience in Web Development.</p>
-                        <div className='mt-8 md:mt-12 flex gap-6 md:gap-10'>
-                            <ul className='text-sm md:text-base flex flex-col gap-4 items-start '>
-                                <li className='font-bold'>Name :</li>
-                                <li className='font-bold'>Date of birth :</li>
-                                <li className='font-bold'>Address :</li>
-                                <li className='font-bold'>Zip code :</li>
-                                <li className='font-bold'>Email :</li>
-                                <li className='font-bold'>Phone :</li>
+                        <p className='mt-6 md:mt-10 md:max-w-[90%] text-gray-400'>Hello! I am Front End Developer. I have one year of experience in Web Development.</p>
+                        <div className='mt-8 md:mt-12 w-auto flex gap-6 md:gap-10'>
+                            <ul className='text-sm md:text-base flex flex-col gap-5 items-start max-w-[30%]'>
+                                <li className='font-bold'>Name:</li>
+                                <li className='font-bold'>Date of birth:</li>
+                                <li className='font-bold'>Address:</li>
+                                <li className='font-bold'>Zip code:</li>
+                                <li className='font-bold'>Email:</li>
+                                <li className='font-bold'>Phone:</li>
                             </ul>
-                            <ul className='flex flex-col gap-4 text-sm md:text-base'>
+                            <ul className='flex flex-col gap-4 text-sm md:text-base max-w-[70%]'>
                                 <li className='text-gray-400'>Muhammad Ammar Shaikh</li>
-                                <li className='text-gray-400'>October 22,2005</li>
-                                <li className='text-gray-400'>Karachi Sindh, Pakistan</li>
+                                <li className='text-gray-400'>October 22,2006</li>
+                                <li className='text-gray-400'>Flat A-10 Pyramid Plaza, Yaseenabad, Karachi Sindh, Pakistan</li>
                                 <li className='text-gray-400'>74000</li>
                                 <li className='text-gray-400'>ammarshaikh50099@gmail.com</li>
                                 <li className='text-gray-400'>+923162324240</li>
                             </ul>
                         </div>
-                        <p className='mt-12 text-xl'><span className='text-yellow-500'>10 </span>Project Complete</p>
+                        <p className='mt-10 text-xl'><span className='text-yellow-500'>10 </span>Project Complete</p>
                         <DownloadButton />
                     </div>
                 </div>
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,14 +2,14 @@
 import Link from 'next/link'
 const Banner = () => {
     return (
-        <section id='home' className='pt-48 relative h-[100vh] md:h-[140vh]' >
-            <div className='absolute bg-bannerImg md:bg-contain bg-cover w-full h-full opacity-30 md:opacity-100 bg-right bg-no-repeat md:-top-4 -top-20 md:right-20'></div>
+        <section id='home' className='pt-48 relative pb-10 ' >
+            <div className='absolute bg-bannerImg md:bg-contain bg-cover w-full h-full opacity-30 md:opacity-100 bg-right bg-no-repeat md:top-0 -top-4  md:right-20'></div>
             <div className='max-w-[96%] md:max-w-[92%] mx-auto'>
                 <div className='flex justify-between'>
                     <div className='w-full md:text-start text-center mt-12 md:mt-0 slide-top' >
                         <span className='text-sm text-yellow-500 ml-1'>HELLO!</span>
                         <h1 className=' md:max-w-[50%] text-6xl mt-6 leading-tight font-extrabold'>Fast, Reliable, and Secure <span className='text-yellow-500'>Web Solutions</span></h1>
-                        <div className='flex items-center gap-2 mt-4 justify-center md:justify-start'>
+                        <div className='flex items-center gap-2 md:mt-6 mt-8 justify-center md:justify-start'>
                             <button className='text-sm text-black w-[110px] h-[50px] p-2 bg-yellow-500 rounded-3xl font-semibold'><Link href="/about">Hire Me</Link></button>
                             <button className='text-sm text-white w-[130px] h-[50px] p-2 bg-transparent border border-white rounded-3xl'><Link href="/project">My Work</Link></button>
                         </div>
